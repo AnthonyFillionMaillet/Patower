8d3b79c0e9462aea6899b3d64ffd66f8ea7950cb
dark-aw <alex9595>
2015-05-01 22:24:04 +0200

    Commentaire + score

--- core/src/com/npngstudio/patower/States/GameScreen.java
+++ core/src/com/npngstudio/patower/States/GameScreen.java
@@ -21,8 +21,10 @@ public class GameScreen extends State {
 	ArrayList<Color> ArrayColor = new ArrayList<Color>();
 	final int DROITE = 0;
 	final int GAUCHE = 1;
+	final int STOP = 2;
 	int RandDirection;
 	int vitesse = 16;
+	int g_Score = 0;
 
 	public GameScreen(GSM p_Gsm){
 		super(p_Gsm);
@@ -32,14 +34,12 @@ public class GameScreen extends State {
 		initRectangle();
 	}
 
+	// Génération de la colonne de base
 	public void initRectangle(){
-
-
 		for (int i = 0; i < 5; i++)
 		{
 			G_ArrayRect.add(new Rectangle(160, i * 80, 160, 80, getColorRand(), false));
 		}
-
 	}
 
 	public void initColor()
@@ -51,6 +51,7 @@ public class GameScreen extends State {
 		ArrayColor.add(Color.MAGENTA);
 	}
 
+	// Permet de récupérer une couleur aléatoirement dans la liste des couleurs
 	public Color getColorRand()
 	{
 		int random = (int)(Math.random()*ArrayColor.size());
@@ -71,13 +72,19 @@ public class GameScreen extends State {
 	
 	public void handleInput() {
 		if(Gdx.input.justTouched()){
+			// Autorise la descente du bloc
 			G_ArrayRect.get(G_ArrayRect.size()-1).setDansLeVide(true);
-			RandDirection = 3;
+			RandDirection = STOP;
 		}
 	}
 	
 	public void update(float p_DelTem) {
 		handleInput();
+
+		if(g_Score % 5 == 0 && g_Score != 0)
+		{
+			vitesse ++;
+		}
 
 		//INTERSECT
 		for(int i = 1; i < G_ArrayRect.size(); i++) {
@@ -107,32 +114,47 @@ public class GameScreen extends State {
 			}
 		}
 
+		// Quand la colonne a perdu un bloc, on en fait spawn un nouveau
 		if(G_ArrayRect.size() == 5){
 			spawnRectangle();
 		}
 
+		// Quand le bloc touche le haut de la colonne
 		if(!G_ArrayRect.get(G_ArrayRect.size()-1).isDansLeVide() && G_ArrayRect.get(G_ArrayRect.size()-1).getY() < 650){
+			// On ajoute un point au score
+			g_Score++;
+			// Pour chaque rectangle
 			for(Rectangle r : G_ArrayRect){
+				// On fait descendre la colonne
 				r.setY(r.getY() - vitesse / 2);
+				// Quand le dernier bloc à disparu de l'écran
 				if(G_ArrayRect.get(1).getY() == 0){
+					// On fait descendre le reste de la colonne
 					for(int j = 2; j < G_ArrayRect.size(); j++){
 						G_ArrayRect.get(j).setY(G_ArrayRect.get(j).getY() - vitesse / 2);
 					}
+					// On supprime le bloc qui a disparu (de l'écran) de la liste
 					G_ArrayRect.remove(0);
+					// Stop la descente de la colonne
 					break;
 				}
 			}
 
 		}
 
+		// Fait bouger à droite ou à gauche le bloc qui a spawn
 		if(RandDirection == GAUCHE){
+			// Fait avancer le bloc de gauche à droite
 			G_ArrayRect.get(G_ArrayRect.size()-1).setX(G_ArrayRect.get(G_ArrayRect.size()-1).getX() + vitesse);
+			// Quand le bloc sort de l'écran, le joueur perd
 			if(G_ArrayRect.get(G_ArrayRect.size()-1).getX() > 480){
 				gsm.push(new MenuScreen(gsm));
 			}
 		}
 		else if (RandDirection == DROITE){
+			// Fait avancer le bloc de droite à gauche
 			G_ArrayRect.get(G_ArrayRect.size()-1).setX(G_ArrayRect.get(G_ArrayRect.size()-1).getX() - vitesse);
+			// Quand le bloc sort de l'écran, le joueur perd
 			if(G_ArrayRect.get(G_ArrayRect.size()-1).getX() + G_ArrayRect.get(G_ArrayRect.size()-1).getWidth() < 0){
 				gsm.push(new MenuScreen(gsm));
 			}
@@ -143,11 +165,7 @@ public class GameScreen extends State {
 
 		p_SprBat.setProjectionMatrix(cam.combined);
 		sr.setProjectionMatrix(cam.combined);
-
-
 		sr.begin(ShapeRenderer.ShapeType.Filled);
-
-
 
 		for(Rectangle rec : G_ArrayRect) {
 			rec.update();
@@ -156,10 +174,7 @@ public class GameScreen extends State {
 		}
 
 		sr.end();
-
 		p_SprBat.begin();
-
-
 		p_SprBat.end();
 
 	}
