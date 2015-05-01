f0f27167b803be578e70bbd36f082b6cbeb5175a
Fifireunionnais <fillioan.info@gmail.com>
2015-05-01 12:33:10 +0200

    Red rectangle

--- core/src/com/npngstudio/patower/States/GameScreen.java
+++ core/src/com/npngstudio/patower/States/GameScreen.java
@@ -1,16 +1,20 @@
 package com.npngstudio.patower.States;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.npngstudio.patower.StatesManager.GSM;
 import com.npngstudio.patower.StatesManager.State;
 
 
+
 public class GameScreen extends State {
-	
+
+	ShapeRenderer sr;
 
 	public GameScreen(GSM p_Gsm){
 		super(p_Gsm);
-
+		sr = new ShapeRenderer();
 	}
 	
 	public void handleInput() {
@@ -24,6 +28,12 @@ public class GameScreen extends State {
 	public void render(SpriteBatch p_SprBat) {
 
 		p_SprBat.setProjectionMatrix(cam.combined);
+
+		sr.begin(ShapeRenderer.ShapeType.Filled);
+		sr.setColor(Color.RED);
+		sr.rect(0, 0, 350, 350);
+		sr.end();
+
 		p_SprBat.begin();
 
 
